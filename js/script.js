db757e8343b8b0328336b9f886f65c3ffc392340
Agathe L <lenclen.agathe@gmail.com>
2019-05-01 23:10:28 +0200

    Move js to a js folder

---
+++ js/script.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWdhdGhlYmx1ZXMiLCJhIjoiY2p2NW5mdGhjMHIyZTN5cG1wdTU5a2ppZiJ9.mGUzsVIk2x1XDXy3Zgp9eA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  minZoom: 11,
+  maxZoom: 13,
+  center: [4.8939090868191215, 52.36163000690422],
+  zoom: 12,
+  bearing: 0,
+  style: mapStyle
+}
+
+);
